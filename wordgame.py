45119ede1216303dc31424e06849391db5400fa7
Thomashu077549 <142924260+thomas077549@users.noreply.github.com>
2024-02-05 20:20:09 -0800

    Create wordgame.py

---
+++ wordgame.py
@@ -0,0 +1,18 @@
+import os
+import csv
+import sys
+import random
+
+def main()
+#print out menu with viable options "selection 1", "selection 2", "selection 3", "random selection"
+#direct to individual functions
+#ask whether user wants to exit or not
+
+#selection individual functions:
+#read file into dictionary
+#assign keys "word" and "selection" to different blocks of information in the dict array
+#randomly pick a word as "answer"
+#record length of answer
+#use while loop to assess the answer for length-1 times
+#if yielded answer, print answer and contratulation
+#if did not yield answer, print answer and direct back to menu
